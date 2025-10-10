0273ede1dc9a878cbb7267284951782541d5201c
Alexander Kraemling <alexander.kraemling@gmail.com>
2025-10-10 14:34:56 -0400

    Fixed for deploy

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -4,7 +4,7 @@ import Game from "@/components/Game";
 const Index = () => {
     return (
         <div>
-            <Game deck={["h1", "c8", "d2", "d4", "s3"]} onPlay={() => null} onPlayEnd={ () => null } />
+            <Game />
         </div>
     );
 };
