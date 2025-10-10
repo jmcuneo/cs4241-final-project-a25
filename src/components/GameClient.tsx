32693b1f87ef0caca6a62086999537667890a71f
Batyrkhan Saparuly <157060867+Han-Saparuly@users.noreply.github.com>
2025-10-10 17:34:00 -0400

    Revert "Added forfeit, working disconnect, other changes"
    This reverts commit 2cd68cea5558615414f6aef0d0307b3f21360de3.

--- src/components/GameClient.tsx
+++ src/components/GameClient.tsx
@@ -2,14 +2,13 @@
 import React, {useEffect, useState} from "react";
 import PlayerArea from "@/components/PlayerArea";
 import {Card} from "@/app/types";
-import {router} from "next/client";
 
 type DeckTheme = "nordic" | "egyptian"; // Since GameClient now passes the themes to PlayerArea
 
 type GameClientProps = {
     deck: string[];
     onPlay: () => void;
-    onForfeit: () => void;
+    onPlayEnd: () => void;
     playerName: string;
     opponentName: string;
     gameStatus?: string;
@@ -31,7 +30,7 @@ function convertToCard(code?: string): Card {
 export default function GameClient({
                                        deck,
                                        onPlay,
-                                       onForfeit,
+                                       onPlayEnd,
                                        playerName,
                                        opponentName,
                                        gameStatus,
@@ -102,12 +101,6 @@ export default function GameClient({
                     backTheme={deckTheme}
                 />
                 <p className="text-center text-sm mt-1">{playerName} ({opponentDeckCount})</p>
-                <button type="button"
-                        onClick={() => {
-                            onForfeit();
-                        }}
-                        className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Forfeit
-                </button>
             </div>
 
             <style jsx global>{`
